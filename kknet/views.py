07e4b763a37644faeb53fb92b9700865fa1e4779
dain <dain7362@gmail.com>
2024-08-02 12:45:43 +0900

    views change

--- kknet/views.py
+++ kknet/views.py
@@ -10,3 +10,27 @@ def ice_cream_list(request):
     return render(request, 'ice_cream_list.html', {
 
     })
+
+def search_view(request):
+    category = request.GET.get('category', 'all')
+    search_query = request.GET.get('search', '')
+
+    if category == 'all':
+        if search_query:
+            images = Ice.objects.filter(name__icontains=search_query)
+        else:
+            images = Ice.objects.all()
+    else:
+        if search_query:
+            images = Ice.objects.filter(category=category, name__icontains=search_query)
+        else:
+            images = Ice.objects.filter(category=category)
+
+    image_url_prefix = "/static/images/"
+
+    return render(request, 'review_list.html', {
+        'images': images,
+        'image_url_prefix': image_url_prefix,
+        'selected_category': category,
+        'search_query': search_query,
+    })
